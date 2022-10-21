66d355c87a3818e5ac2d31e8ca5e3644c79617d5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-21 12:10:25 +0600

    register page

--- frontend/src/page/Register/Register.js
+++ frontend/src/page/Register/Register.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
 import './Register.css'
 
 const Register = () => {
     return (
         <div>
-            
+        <Navbar/>
+       <div className='regContainer'>
+        <div className='regWrapper'>
+            <div className='regHeader'>
+             <h2>Register here</h2>
+            </div>
+            <div className='regForm'>
+                <form className='regFormInp'>
+                    <lable>Name: </lable>
+                    <input name='username' type="text"/>
+                    <br/>
+                    <lable>Email: </lable>
+                    <input name='email' type="email"/>
+                    <br/>
+                    <lable>Password: </lable>
+                    <input name='password' type="password"/>
+                    <br/>
+                    <lable>Admin: </lable>
+                    <input name='isAdmin' type="Boolean"/>
+                     
+                </form>
+                <button>Submit</button>
+                <p>Already have an account?<Link to='/login'><span>SignIn</span></Link></p>
+            </div>
+
         </div>
+
+       </div>
+    </div>
     );
 };
 
