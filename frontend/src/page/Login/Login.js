3835be52c539585ea598f87108bddbad5492b71c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-21 12:03:52 +0600

    login page

--- frontend/src/page/Login/Login.js
+++ frontend/src/page/Login/Login.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
 import './Login.css'
+import logo from '../../images/logo.png'
 
 const Login = () => {
     return (
         <div>
+            <Navbar/>
+            <div className='loginContainer' >
             
+                <img className='loginImg' src={logo}/>
+                
+                 <div className='loginWrapper'>
+                   <div className='loginHeader'>
+                   <h2>Login With</h2>
+                   </div>
+                   <div className='loginInput'>
+                     <div className='loginInputFiel'>
+                       <input name='username'className='loginInputField' type="text" placeholder="username"/>
+                         <br/>
+                        <input name='password' className='loginInputField' type="password" placeholder="password"/>
+                        <br/>
+                       </div>
+                    
+                      <button className='loginInputField'>Submit</button>
+                   </div>
+                   <div className='loginLink'>
+                    <p>Don't have an accont?<Link to="/register"><span className='loginSpan'>Create an account</span></Link></p>
+                   </div>
+                   
+                 </div>
+
         </div>
+
+    </div>
     );
 };
 
