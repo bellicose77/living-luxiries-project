94cd44673048a83f74772c5ad65fded1b5fcbdd2
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-21 12:08:26 +0600

    register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./page/Home/Home";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from "./page/Login/Login";
+import Register from "./page/Register/Register";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   
