3e45078e221a22d5acf5f164e77e0e078dbb2da8
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-22 16:40:16 +0600

    database connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 6000;
 
+const connect = async() =>{
+    try{
+
+    }catch(e){
+        next(e)
+    }
+
+}
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
