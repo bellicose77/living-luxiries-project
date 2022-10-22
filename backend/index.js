33d9539a1707b29a9b42947cbed519e397838523
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-22 16:47:01 +0600

    database connection done

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const { default: mongoose } = require('mongoose');t
+const { default: mongoose } = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors')
 const app = express();
+dotenv.config();
 const port = 6000;
 
 // data base connection 
 const connect = async() =>{
     try{
-      await mongoose.connect(process.env.MONGO)
+      await mongoose.connect(process.env.MONGO);
+      console.log("database connected")
     }catch(e){
-        next(e)
+        throw(e)
     }
 
 }
 
 app.listen(port, () => {
+    connect()
     console.log(`Example app listening on port ${port}`)
   })
