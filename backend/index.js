2d14c1eda165f388859b0ff24346bbc1192d5058
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-22 16:43:38 +0600

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');t
+const dotenv = require('dotenv');
+const cors = require('cors')
 const app = express();
 const port = 6000;
 
+// data base connection 
 const connect = async() =>{
     try{
-
+      await mongoose.connect(process.env.MONGO)
     }catch(e){
         next(e)
     }
