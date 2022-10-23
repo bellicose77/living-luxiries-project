75828ac7cf224d7d66dc0b99cde7d5ab6ce219b1
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-23 11:27:24 +0600

    import way change

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,9 @@
-const User = require('../models/User')
-export const registerController = async(req,res,next)=>{
+const User = require('../models/User');
+
+ export const registerController = async(req,res,next)=>{
     try{
+        const {name,email,password} = req.body;
+        console.log(name);
 
     }catch(e){
         next(e);
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express'
 const { default: mongoose } = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
