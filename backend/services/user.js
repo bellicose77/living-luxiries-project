a3859909a7538f523dd668f566c9159fc9d621a3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-01 21:20:48 +0600

    user find function

--- backend/services/user.js
+++ backend/services/user.js
@@ -1,1 +1,9 @@
-const
+import User from "../models/User"
+
+export const findUserByProperty = (key,value) =>{
+    if(key=='_id'){
+        return User.findById()
+    }
+    return User.find({email:value});
+
+}
