086732a63c8fb04634a7908c6b1e1bad07a341eb
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-01 20:33:53 +0600

    global error handling

--- backend/utils/error.js
+++ backend/utils/error.js
@@ -1,3 +1,7 @@
-export const error = (message,status) =>{
-    
+export const error = (message="Something happend in server code",status=500) =>{
+
+    const e = new Error(message);
+    e.status=status;
+    return e;
+
 }
