0f1253ee25e1dc3868e0ae035df2a7244e0c2c87
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-06 19:54:11 +0600

    authoraization api

--- backend/middelwares/checklogin.js
+++ backend/middelwares/checklogin.js
@@ -1,6 +1,7 @@
 export const checkLogin = async (req,res,next)=>{
     const {authoraization} = req.headers;
     const token = authoraization.split[1];
-    console.log("from middelware",token)
+    console.log("from middelware",token);
+    next();
     
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { homeController, logingController, registerController } from '../controllers/auth.js';
+import { checkLogin } from '../middelwares/checklogin.js';
 
 const router = express.Router();
 
 router.post('/register',registerController);
 router.post('/login',logingController);
-router.get('/home',homeController);
+router.get('/home',checkLogin, homeController);
 
 export default router;
