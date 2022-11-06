9bf992b2db2a7ea4ca9b809be8baf29f49075d3f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-06 20:16:53 +0600

    authorization

--- backend/middelwares/checklogin.js
+++ backend/middelwares/checklogin.js
@@ -1,10 +1,15 @@
 import jwt from 'jsonwebtoken';
+import { error } from '../utils/error.js';
 export const checkLogin = async (req,res,next)=>{
     const {authorization} = req.headers;
     const token = authorization.split(" ")[1];
-
-    const verify = await jwt.verify(token,process.env.SECRET_KEY);
-    console.log(verify);
-    next();
+    try{
+        const verify = await jwt.verify(token,process.env.SECRET_KEY);
+        if(verify){
+            return res.status(200).json(verify)
+        }
+    }catch(err){
+        next(err);
+    }
     
 }
