3a0a1b227f6ca9bc730584de471baf7054c19a33
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-06 19:42:27 +0600

    authoraization middelware

--- backend/middelwares/checklogin.js
+++ backend/middelwares/checklogin.js
@@ -1,3 +1,6 @@
-export const checkLogin = ()=>{
+export const checkLogin = async (req,res,next)=>{
+    const {authoraization} = req.headers;
+    const token = authoraization.split[1];
+    console.log("from middelware",token)
     
 }
